b13f791bf9838909c389547dea5179cbefb2a078
frillizard <rikkisasda@gmail.com>
2018-10-06 11:19:36 -0500

    youtube service functional

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,16 @@
 angular.module('video-player')
 .component('app', {
   templateUrl: 'src/templates/app.html',
-  controller: function() {
+  controller: function(youTube) {
     this.videos = exampleVideoData;
     this.currentVideo = exampleVideoData[0];
-    this.changeVideoOnClick = function(video) {
+    this.selectVideo = function(video) {
       this.currentVideo = video;
     };
-    this.onClick = this.changeVideoOnClick.bind(this);
+    this.onClick = this.selectVideo
+      .bind(this);
+    let testObject = {query:'cats', key: window.YOUTUBE_API_KEY};
+    console.log('Testing with ', testObject);
+    youTube.search(testObject, () => true);
   }
 });
--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,4 +1,25 @@
 angular.module('video-player')
-.service('youTube', function(){
-  // TODO
+.service('youTube', function($http) {
+  this.search = function(params, callback) {
+    if (params.maxResults === undefined) {
+      params.maxResults = 5;
+    }
+    let sentData = {
+      //part: 'snippet',
+      q: params.query,
+      maxResults: params.maxResults,
+      //key: params.key,
+      videoEmbeddable: 'true'
+    }; 
+    console.log('Sending ', sentData);
+    $http({
+      method: 'GET',
+      url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&key=' + params.key,
+      data: sentData
+    }).then(function successCallback(response) {
+      console.log('Data recieved:', response);
+    }, function errorCallback(response) {
+      console.log('Server error: ', response);
+    });
+  };
 });
